8310d5853bbe9cd222cece70f235201957d6e025
Mario Hero <mariocanfly@126.com>
2024-10-19 13:41:57 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
-#include <unordered_map>
 #include <string>
 
+std::string MacCompony(const int macDigit){
+	switch (macDigit){
+		// ***insert_point***
+		case 123: return "good";
+		// ***insert_point***
+		default: return "unknown";
+	}
+	return "unknown";
+}
+
 int main(int argc, char* argv[]) {
-	// ***insert_point***
-	std::unordered_map<int, std::string> oui_data = {
-		{1108370,"INGRAM MICRO SERVICES"}
-	};
-	// ***insert_point***
 	for (int i = 1; i < argc; ++i) {
 		const std::string mac = std::string(argv[i]);
-		int mac_digit = 0;
+		int macDigit = 0;
 		int addTimes = 0;
 		for(char c: mac){
 			if (isalnum(c)) {
 				c = toupper(c);
 				if (c >= 'A' && c <= 'F') {
-					mac_digit = mac_digit * 16 + 10 + c - 'A';
+					macDigit = macDigit * 16 + 10 + c - 'A';
 				}
 				else {
-					mac_digit = mac_digit * 16 + c - '0';
+					macDigit = macDigit * 16 + c - '0';
 				}
 				addTimes++;
 				if (addTimes >= 6) {
@@ -27,14 +31,8 @@ int main(int argc, char* argv[]) {
 				}
 			}
 		}
-		
-		const auto it = oui_data.find(mac_digit);
-		if (it != oui_data.end()) {
-			std::cout << mac << " " << it->second << std::endl;
-		}
-		else {
-			std::cout << mac << " unknown" << std::endl;
-		}
+		const std::string ans = MacCompony(macDigit);
+		std::cout << mac << " " << ans << std::endl;
 	}
 	return 0;
 }
